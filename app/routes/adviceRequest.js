b097f048542661606fd2b97b57a3423e8a1f9511
romaissalmh <hr_kessi@esi.dz>
2021-07-11 15:34:30 +0100

    deleted token

--- app/routes/adviceRequest.js
+++ app/routes/adviceRequest.js
@@ -5,7 +5,7 @@ var {verifyTokenPatient, verifyTokenDoctor} = require('./verifyToken')
 
 var router= express.Router(); 
 
-router.post('/',verifyTokenPatient,adviceReqController.createAdviceRequest);
+router.post('/',/*verifyTokenPatient,*/adviceReqController.createAdviceRequest);
 router.get('/', adviceReqController.getAllAdviceRequests);
 router.get('/byDoctor/:id',verifyTokenDoctor, adviceReqController.getAllAdviceRequestsByDoctor);
 router.get('/byPatient/:id',/*verifyTokenPatient,*/ adviceReqController.getAllAdviceRequestsByPatient);
